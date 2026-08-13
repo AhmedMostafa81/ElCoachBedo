01e6e188b410f29a234fc929f6d0da5195d9ab5a
AhmedPlusPlus <157212041+AhmedMostafa81@users.noreply.github.com>
2026-08-13 19:07:47 +0300

    Update matrix exponentiation.cpp

--- Math & probability/matrix exponentiation.cpp
+++ Math & probability/matrix exponentiation.cpp
@@ -1,83 +1,45 @@
-
-matrix mul(matrix &a , matrix &b)
-{
-    int n = a.size() , m = b[0].size();
-    matrix res = zero(n , m);
-
-    for(int i=0; i<n; i++)
-    {
-        for(int j=0; j<m; j++)
-        {
-            for(int k=0; k<n; k++)
-            {
-                res[i][j] += a[i][k]*b[k][j];
-                res[i][j] %= mod;
-            }
-        }
-    }
+#include <bits/stdc++.h>
+#define matrix std::vector<std::vector<long long>>
+#define zero(n, m) matrix(n, std::vector<long long>(m))
+const int mod = 1e9+7;
+matrix mul(matrix &a, matrix &b){
+    int n = a.size(), m = b[0].size(), l = b.size();
+    assert(l != a[0].size());
+    matrix res = zero(n, m);
+    for(int i=0; i<n; ++i)
+        for(int j=0; j<m; ++j)
+            for(int k=0; k<n; ++k)
+                res[i][j] = (res[i][j] + a[i][k]*b[k][j]) % mod;
     return res;
 }
-
-//     floyd
-
-#include<bits/stdc++.h>
-using namespace std;
-#define int long long
-typedef vector<vector<int>> matrix;
-#define init(name,n,m,x) matrix name (n,vector<int>(m,x))
-#define AhmedPlusPlus ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-#define hi cerr<<"HI\n";
-
-/*                        ->    NO CLEAN CODE HERE    <-                        */
-
-const int inf = 3e18;
-
-matrix mul(matrix a , matrix &b)
-{
-    int n = a.size() , m = b[0].size();
-    init(res, n,m,inf);
-    for(int i=0; i<n; i++)
-    {
-        for(int j=0; j<m; j++)
-        {
-            for(int k=0; k<n; k++)
-            {
-                res[i][j] = min(res[i][j],a[i][k]+b[k][j]);
-            }
-        }
-    }
+matrix matrix_fp(matrix &b, long long e){
+    if(e == 1) return b;
+    matrix res = matrix_fp(b, e>>1);
+    res = mul(res, res);
+    if(e&1) res = mul(res, b);
     return res;
 }
-
-matrix matrix_fp(matrix &a , int p)
-{
-    if (p==1)
-        return a;
-
-    matrix res = matrix_fp(a , p/2);
-    res = mul(res , res);
-    if (p%2) res = mul(res , a);
-
-    return res;
+void base_case(matrix &dp){
+    dp[0][0] = 0;
+    dp[1][0] = 1;
 }
-
+void transition(matrix &T){
+    T[0][0] = 0;
+    T[0][1] = 1;
+    T[1][0] = 1;
+    T[1][1] = 1;
+}
 signed main() {
-/* ^^^ */    AhmedPlusPlus    /* ^^^ */
-
-//    ->> practice makes perfect
-
-    int n,m,k;cin>>n>>m>>k;
-    init(base_case,n,1,0);
-    init(T,n,n,inf);
-    for(int i = 0,x,y,w;i <m;i++){
-        cin>>x>>y>>w;
-        x--,y--;
-        T[y][x] = min(T[y][x],w);
+    std::ios_base::sync_with_stdio(false); std::cin.tie(nullptr);
+    long long n;
+    std::cin >> n;
+    if(!n) std::cout << n << '\n';
+    else{
+        matrix T = zero(2, 2);
+        matrix dp = zero(2, 1);
+        base_case(dp);
+        transition(T);
+        T = matrix_fp(T, n);
+        std::cout << mul(T, dp)[0][0] << '\n';
     }
-    matrix res = mul(matrix_fp(T,k),base_case);
-    int ans = inf;
-    for (int i =0;i<n;i++)ans = min(ans , res[i][0]);
-    if (ans > 2e18+5)cout << "IMPOSSIBLE" ;
-    else cout << ans << '\n';
-
 }

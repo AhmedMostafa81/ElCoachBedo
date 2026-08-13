3372973bb203dffcc6f209bef70606913410c91d
AhmedPlusPlus <157212041+AhmedMostafa81@users.noreply.github.com>
2026-08-13 19:03:41 +0300

    Add CHT implementation in Tricks/CHT.cpp
    Implemented a LineContainer structure for convex hull trick.

---
+++ Tricks/CHT.cpp
@@ -0,0 +1,42 @@
+#include <assert.h>
+typedef long long ll;
+struct Line {
+    ll k, m;
+    mutable ll p;
+    bool operator<(const Line& o) const {
+        return k < o.k;
+    }
+    bool operator<(const ll&x) const {
+        return p < x;
+    }
+};
+
+// y = k*x + m
+struct LineContainer : std::multiset<Line, std::less<>> {
+    // (for doubles, use inf = 1/.0, div(a,b) = a/b)
+    const ll inf = std::numeric_limits<ll>::max();
+    ll div(ll a, ll b) { // floored division
+        return a / b - ((a ^ b) < 0 && a % b);
+    }
+    bool isect(iterator x, iterator y) {
+        if (y == end()) { x->p = inf; return false; }
+        if (x->k == y->k) x->p = x->m > y->m ? inf : -inf;
+        else x->p = div(y->m - x->m, x->k - y->k);
+        return x->p >= y->p;
+    }
+    void add(ll k, ll m) {
+        auto z = insert({k, m, 0}), y = z++, x = y;
+        while (isect(y, z)) z = erase(z);
+        if (x != begin() && isect(--x, y)) isect(x, y = erase(y));
+        while ((y = x) != begin() && (--x)->p >= y->p)
+            isect(x, erase(y));
+    }
+    ll query(ll x) {
+        assert(!empty());
+        auto l = *lower_bound(x);
+        return l.k * x + l.m;
+    }
+    // to find the minimum
+    // add(-k, -m)
+    // query(x) * -1
+};
